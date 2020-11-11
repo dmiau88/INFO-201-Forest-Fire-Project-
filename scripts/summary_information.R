c70e910830ee1874b90e008e0fea9b24e1eb1468
Daniel Miau <dmiau88@uw.edu>
2020-11-11 12:47:56 -0800

    Updated summary information

--- scripts/summary_information.R
+++ scripts/summary_information.R
@@ -6,53 +6,29 @@ library("stringr")
 fires <- read.csv("..\\data\\California_Fire_Incidents.csv")
 View(fires)
 
-# Variable to find average number of acres burned per year
+# Total number of fires in the dataset 
 
-avg_acres_burned <- fires %>%
-  group_by(ArchiveYear) %>%
-  summarize(avg_acres = round(mean(AcresBurned, na.rm = TRUE)))
+total_num_fires <- nrow(fires)
 
-# Variable to find average number of fires per county per year
+# Total number of features for the dataset
 
-avg_num_fires_counties <- fires %>%
-  group_by(ArchiveYear, Counties) %>%
-  summarize(avg_fires_county = round(mean(AcresBurned, na.rm = TRUE)))
+total_num_features <- ncol(fires)
 
-# Variable to find average amount of days it took to contain each fire per year
-fires <- fires %>%
-  mutate(
-    Extinguished = substr(Extinguished, 1, 10),
-    Started = substr(Started, 1, 10)
-  ) %>%
-  mutate(
-    Extinguished = as.Date(Extinguished, "%Y-%m-%d"),
-    Started = as.Date(Started, "%Y-%m-%d")
-  )
+# How many years does the dataset show?
 
-avg_time_contain <- fires %>%
-  mutate(Date_Difference = Extinguished - Started) %>%
-  filter(Date_Difference >= 0) %>%
-  group_by(ArchiveYear) %>%
-  summarize(avg_day_diff = round(mean(Date_Difference, na.rm = TRUE)))
+featured_years <- fires %>% 
+  mutate(yearDiff = max(ArchiveYear) - min(ArchiveYear)) %>% 
+  filter(Started == min(Started))
 
-# Variable to find total amount of days it took to contain all fires each year.
+featured_years <- head(featured_years$yearDiff, 1)
 
-sum_time_contain <- fires %>%
-  mutate(Date_Difference = Extinguished - Started) %>%
-  filter(Date_Difference >= 0) %>%
-  group_by(ArchiveYear) %>%
-  summarize(sum_day_diff = round(sum(Date_Difference, na.rm = TRUE)))
+# How many unique counties are in the dataset? 
+num_unique_counties <- length(unique(fires$Counties))
 
+# What is the largest fire in the dataset?
+largest_fire <- fires %>% 
+  filter(AcresBurned == max(AcresBurned, na.rm = TRUE))
 
-# Variable to find total number of fires per year
+largest_fire <- head(largest_fire$Name, 1)
 
-total_num_fires <- fires %>%
-  group_by(ArchiveYear) %>%
-  summarize(total_num_per_year = n())
-
-# Variable to find county with highest number of fires per year
-
-county_highest_fires <- fires %>%
-  group_by(ArchiveYear, Counties) %>%
-  summarize(county_num_per_year = max(n())) %>%
-  filter(county_num_per_year == max(county_num_per_year))
+View(largest_fire)
