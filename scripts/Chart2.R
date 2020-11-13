aaa36fa863a9c99be114cccf48ba17772a20522e
Daniel Miau <dmiau88@uw.edu>
2020-11-12 18:49:27 -0800

    Work in progress

--- scripts/Chart2.R
+++ scripts/Chart2.R
@@ -4,7 +4,7 @@ library(ggplot2)
 ggplot(data=total_num_fires) + 
   geom_col(mapping = aes(x=ArchiveYear, y=total_num_fires)) +
   labs(
-    title = "Total Number of Fires Each Year"
+    title = "Total Number of Fires Each Year",
     x = "Year",
     y = "Number of Fires"
   )
