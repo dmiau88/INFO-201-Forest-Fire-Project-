fe3eae31a4abcce62f4b69a554e8b6f3066103ec
andrwwong <andrewclintonwong@gmail.com>
2020-11-12 18:29:40 -0800

    Fixed chart 2

--- scripts/Chart2.R
+++ scripts/Chart2.R
@@ -2,9 +2,9 @@ library(dplyr)
 library(ggplot2)
 
 ggplot(data=total_num_fires) + 
-  geom_col(mapping = aes(x=ArchiveYear, y=total_num_fires)) +
+  geom_col(mapping = aes(x=ArchiveYear, y=total_num_per_year)) +
   labs(
-    title = "Total Number of Fires Each Year"
+    title = "Total Number of Fires Each Year",
     x = "Year",
     y = "Number of Fires"
   )
