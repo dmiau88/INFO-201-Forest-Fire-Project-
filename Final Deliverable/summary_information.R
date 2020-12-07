fea053b97e9d8667906960295c1d69e42df83726
Kyle Tsang <tsang285@uw.edu>
2020-12-06 22:45:37 -0800

    Final Deliverable work in progress

---
+++ Final Deliverable/summary_information.R
@@ -0,0 +1,32 @@
+library("tidyverse")
+library("dplyr")
+library("stringr")
+
+# Function for reading the dataset of California fires.
+fires <- read.csv("..\\data\\California_Fire_Incidents.csv")
+View(fires)
+
+# Total number of fires in the dataset
+
+total_num_fires <- nrow(fires)
+
+# Total number of features for the dataset
+
+total_num_features <- ncol(fires)
+
+# How many years does the dataset show?
+
+featured_years <- fires %>%
+  mutate(yearDiff = max(ArchiveYear) - min(ArchiveYear)) %>%
+  filter(Started == min(Started))
+
+featured_years <- head(featured_years$yearDiff, 1)
+
+# How many unique counties are in the dataset?
+num_unique_counties <- length(unique(fires$Counties))
+
+# What is the largest fire in the dataset?
+largest_fire <- fires %>%
+  filter(AcresBurned == max(AcresBurned, na.rm = TRUE))
+
+largest_fire <- head(largest_fire$Name, 1)
---
+++ Final Deliverable/ui.R
@@ -0,0 +1,162 @@
+library(shiny)
+library(tidyverse)
+library(plotly)
+library(lintr)
+library(styler)
+library(RColorBrewer)
+
+fires <- read.csv("../data/California_Fire_Incidents.csv")
+
+#Intro Page
+
+Introduction <- tabPanel(
+  "Introduction",
+  mainPanel(
+    h1("Introductory Page"),
+    p("Welcome to my assignment 4! For my assignment, I have chosen to track the variables of greenhouse gases,
+      coal, and CO2 using a data set compiled by Our World in Data (link below). Through working with
+      the data, I have looked to find relevant values and trends within the data that will allow me
+      to put together a greater picture about climate change over time, and how differnet countries
+      (my work focuses on the USA), has impacted climate change throughout history."),
+    p(a("Our World in Data", href = "https://ourworldindata.org/co2-and-other-greenhouse-gas-emissions")),
+    p(strong("Below are 5 relevant data points about climate change that I calculated
+             using the raw data compiled by Our World in Data:")),
+    p("The difference between the USA CO2 emissions from coal in the years 2000 and 2010 was 159.117 million tonnes"),
+    p("The year where the USA's share of global CO2 emissions was the greatest was in 2000"),
+    p("In the most recent documented year for this metric (2014), the USA's greenhouse gas
+      emissions per capita was 19.135 tonnes"),
+    p("After charging the USA's greenhouse gas emissions each year, I found that the greatest emissions
+      came in 2007, at 6601.13 million tonnes of carbon dioxide equivalents."),
+    p("In 1990, the United States had the greatest cumulative share of global CO2 emissions at 30.716% of global emissions.
+      This emissions number was also the greatest cumulative share of global CO2 emissions within the entire datset.")
+  ))
+
+#Chart 1 Page
+
+ChartPage1 <- tabPanel(
+  "Interactive Visual Page 1 - Counting Number of Fire Occurences by month",
+  sidebarLayout(
+    sidebarPanel(
+      p("Select your viewing options!"),
+      selectInput("country_id",
+                  label = h3("Country"),
+                  choices = totalghg_graphdata$country
+      ),
+      selectInput("color_id",
+                  label = h3("Color"),
+                  choices = brewer.pal(8, "Set2")
+      )
+    ),
+    mainPanel(
+      h2("Climate Change Visualization"),
+      p("Below is my interactive visualization showing the total greenhouse gas emissions for each country"),
+      plotlyOutput(outputId = "country_ghg"),
+      p("The reason why I chose to include this chart was to provide an individualized snapshot of each country's
+        greenhouse gas emissions. Compared to the large data chart (even when you use dplyr to parse some of the
+        data, it can get a little overwhelming. While the data visualilzations does not include functionality to
+        compare countries in terms of greenhouse gas emissions, it still helps to provide a visual illustration
+        of the data")
+    )
+  )
+)
+
+#Chart 2 Page
+
+ChartPage2 <- tabPanel(
+  "Interactive Visual Page 1 - Counting Number of Fire Occurences by month",
+  sidebarLayout(
+    sidebarPanel(
+      p("Select your viewing options!"),
+      selectInput("country_id",
+                  label = h3("Country"),
+                  choices = totalghg_graphdata$country
+      ),
+      selectInput("color_id",
+                  label = h3("Color"),
+                  choices = brewer.pal(8, "Set2")
+      )
+    ),
+    mainPanel(
+      h2("Climate Change Visualization"),
+      p("Below is my interactive visualization showing the total greenhouse gas emissions for each country"),
+      plotlyOutput(outputId = "country_ghg"),
+      p("The reason why I chose to include this chart was to provide an individualized snapshot of each country's
+        greenhouse gas emissions. Compared to the large data chart (even when you use dplyr to parse some of the
+        data, it can get a little overwhelming. While the data visualilzations does not include functionality to
+        compare countries in terms of greenhouse gas emissions, it still helps to provide a visual illustration
+        of the data")
+    )
+  )
+)
+
+
+#Chart 3 Page
+
+ChartPage3 <- tabPanel(
+  "Interactive Visual Page 1 - Containing Fires",
+  sidebarLayout(
+    sidebarPanel(
+      p("Average Time to Contain Fires Per Year Per County"),
+      selectInput("country_id",
+                  label = h3("Country"),
+                  choices = totalghg_graphdata$country
+      ),
+      selectInput("color_id",
+                  label = h3("Color"),
+                  choices = brewer.pal(8, "Set2")
+      )
+    ),
+    mainPanel(
+      h2("Climate Change Visualization"),
+      p("Below is my interactive visualization showing the total greenhouse gas emissions for each country"),
+      plotlyOutput(outputId = "country_ghg"),
+      p("The reason why I chose to include this chart was to provide an individualized snapshot of each country's
+        greenhouse gas emissions. Compared to the large data chart (even when you use dplyr to parse some of the
+        data, it can get a little overwhelming. While the data visualilzations does not include functionality to
+        compare countries in terms of greenhouse gas emissions, it still helps to provide a visual illustration
+        of the data")
+    )
+  )
+)
+
+#Summary Page
+
+SummaryPage <- tabPanel(
+  "Introduction",
+  mainPanel(
+    h1("Introductory Page"),
+    p("Welcome to my assignment 4! For my assignment, I have chosen to track the variables of greenhouse gases,
+      coal, and CO2 using a data set compiled by Our World in Data (link below). Through working with
+      the data, I have looked to find relevant values and trends within the data that will allow me
+      to put together a greater picture about climate change over time, and how differnet countries
+      (my work focuses on the USA), has impacted climate change throughout history."),
+    p(a("Our World in Data", href = "https://ourworldindata.org/co2-and-other-greenhouse-gas-emissions")),
+    p(strong("Below are 5 relevant data points about climate change that I calculated
+             using the raw data compiled by Our World in Data:")),
+    p("The difference between the USA CO2 emissions from coal in the years 2000 and 2010 was 159.117 million tonnes"),
+    p("The year where the USA's share of global CO2 emissions was the greatest was in 2000"),
+    p("In the most recent documented year for this metric (2014), the USA's greenhouse gas
+      emissions per capita was 19.135 tonnes"),
+    p("After charging the USA's greenhouse gas emissions each year, I found that the greatest emissions
+      came in 2007, at 6601.13 million tonnes of carbon dioxide equivalents."),
+    p("In 1990, the United States had the greatest cumulative share of global CO2 emissions at 30.716% of global emissions.
+      This emissions number was also the greatest cumulative share of global CO2 emissions within the entire datset.")
+  ))
+
+#Fluid Page Code
+
+ui <- fluidPage(
+  h1("INFO 201 Assingment 4: Data Applications (Climate Change)"),
+  h2("Kyle Tsang, Section AB"),
+  navbarPage(
+    inverse = TRUE,
+    "Climate Change Assignment",
+    Introduction,
+    ChartPage1,
+    ChartPage2,
+    ChartPage3,
+    SummaryPage
+  )
+)
+
+#Define Server
