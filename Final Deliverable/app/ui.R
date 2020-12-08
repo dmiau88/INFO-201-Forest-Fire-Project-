2dac0f4908eb899cec55c34c025a0bd2ebe9a4ef
Daniel Miau <dmiau88@uw.edu>
2020-12-07 23:58:51 -0800

    Edited introduction page text and changed some navbar titles

--- Final Deliverable/app/ui.R
+++ Final Deliverable/app/ui.R
@@ -14,23 +14,34 @@ fires <- read.csv("https://raw.githubusercontent.com/dmiau88/INFO-201-Forest-Fir
 Introduction <- tabPanel(
   "Introduction",
   mainPanel(
-    h1("Overview of our project"),
-    p("Welcome to my assignment 4! For my assignment, I have chosen to track the variables of greenhouse gases,
-      coal, and CO2 using a data set compiled by Our World in Data (link below). Through working with
-      the data, I have looked to find relevant values and trends within the data that will allow me
-      to put together a greater picture about climate change over time, and how differnet countries
-      (my work focuses on the USA), has impacted climate change throughout history."),
-    p(a("Our World in Data", href = "https://ourworldindata.org/co2-and-other-greenhouse-gas-emissions")),
-    p(strong("Below are 5 relevant data points about climate change that I calculated
-             using the raw data compiled by Our World in Data:")),
-    p("The difference between the USA CO2 emissions from coal in the years 2000 and 2010 was 159.117 million tonnes"),
-    p("The year where the USA's share of global CO2 emissions was the greatest was in 2000"),
-    p("In the most recent documented year for this metric (2014), the USA's greenhouse gas
-      emissions per capita was 19.135 tonnes"),
-    p("After charging the USA's greenhouse gas emissions each year, I found that the greatest emissions
-      came in 2007, at 6601.13 million tonnes of carbon dioxide equivalents."),
-    p("In 1990, the United States had the greatest cumulative share of global CO2 emissions at 30.716% of global emissions.
-      This emissions number was also the greatest cumulative share of global CO2 emissions within the entire datset.")
+    h1(strong("Background and Context")),
+    p("Welcome to our final deliverable! Throughout this entire quarter, 
+      our group has been working on a project relating to the Calfiornian 
+      forest fires. We are interested in this topic because it is very 
+      relevant to current events. While many of our group members experienced
+      the result of the fires near the end of the summer in Seattle, one of
+      our teammates, Andrew Wong, actually lived 30 minutes away from raging
+      forest fires in California. Our project topic of forest fires also 
+      relates to the overarching conversation around climate change and fossil
+      fuel usage. As the nationwide forest fires continue to rage, we hope to
+      find patterns not only in the impact of these fires, but also in their
+      causes."),
+    img(src='forest_fires.png', height = "130px" , width = "400px"),
+    h1(strong("Questions we are looking to answer")),
+    p("The main questions we would like to answer in this project are 
+      1. What is the number of fires that occurred each month from 2013-2018?
+      2. What areas in California were most affected by forest fires?
+      3. What was the total number of fires each year in each county?"),
+    h1(strong("Our Dataset")),
+    p(a("Link to Dataset", href = "https://www.kaggle.com/ananthu017/california-wildfire-incidents-20132020")),
+    p("In order to answer the questions above, we utilized a specific dataset
+      relating to Californian forest fires from Kaggle.com. The data is compiled
+      from the California Department of Forestry and Fire Protection. This set
+      has lots of data regarding where wildfires have occurred (county name,
+      latitude/longitude), and contains details about when the wildfire has 
+      started. There are currently 1637 observations/rows in the data set
+      and 40 features/columns in the data set. We felt that this data set
+      was the most accessible in finding the data that we wanted to explore.")
   ))
 
 #Chart 1 Page
